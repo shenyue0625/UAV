2d14f9dbbfa7de8f8890a757505155b1338f76aa
Shujie Chen <53589550+catling33@users.noreply.github.com>
2021-02-20 16:57:42 -0500

    Update OrderRequestBody.java

--- BackEnd/src/main/java/com/project/autoexpress/requestholder/OrderRequestBody.java
+++ BackEnd/src/main/java/com/project/autoexpress/requestholder/OrderRequestBody.java
@@ -2,4 +2,85 @@ package com.project.autoexpress.requestholder;
 
 public class OrderRequestBody {
 
+    private String senderAddress;
+    private String receiverAddress;
+    private String receiverName;
+    private String cardNumber;
+    private int size;
+    private int weight;
+    private String description;
+    private String deliveryMethod;
+    private float fee;
+
+    public void setSenderAddress(String senderAddress) {
+        this.senderAddress = senderAddress;
+    }
+
+    public void setReceiverAddress(String receiverAddress) {
+        this.receiverAddress = receiverAddress;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDeliveryMethod(String deliveryMethod) {
+        this.deliveryMethod = deliveryMethod;
+    }
+
+    public void setFee(float fee) {
+        this.fee = fee;
+    }
+
+    public String getSenderAddress() {
+        return senderAddress;
+    }
+
+    public String getReceiverAddress() {
+        return receiverAddress;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDeliveryMethod() {
+        return deliveryMethod;
+    }
+
+    public float getFee() {
+        return fee;
+    }
 }
