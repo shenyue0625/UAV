6d6e92034fba9c1a190a154760c33201b948d96d
Shujie Chen <53589550+catling33@users.noreply.github.com>
2021-02-20 16:17:43 -0500

    Revert "test"
    This reverts commit 8471800942a800208b6d0bf203a9e83d78bacf23.

--- BackEnd/src/main/java/com/project/autoexpress/handler/dao/CustomerDao.java
+++ BackEnd/src/main/java/com/project/autoexpress/handler/dao/CustomerDao.java
@@ -29,7 +29,6 @@ public class CustomerDao {
     customer.setFirstName(request.getFirstName());
     customer.setLastName(request.getLastName());
     customer.setUser(user);
-    customer.setId(user);
 
     Authorities authorities = new Authorities();  // build a authorities from request
     authorities.setAuthorities("ROLE_USER");
