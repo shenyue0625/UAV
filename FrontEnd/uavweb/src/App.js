35677f391ec95f1fc4bc8615644dca5de0b5d47a
simon zhang <zhang86036055@gmail.com>
2021-02-21 11:48:21 -0800

    Update App.js

--- FrontEnd/uavweb/src/App.js
+++ FrontEnd/uavweb/src/App.js
@@ -1,73 +1,76 @@
-import React from 'react';
-import { getTrackingDetails } from './utils';
-import Tracking from './components/tracking';
-import './App.css';
-import { Layout, Menu, message } from 'antd';
+import React from "react";
+import { getTrackingDetails } from "./utils";
+import Tracking from "./components/tracking";
+import "./App.css";
+import { Layout, Menu, message } from "antd";
 
+import Ordering from "./components/ordering";
 
 const { Header, Content, Footer } = Layout;
 
 class App extends React.Component {
   state = {
-      orderId: '12345',
-      senderAddress:'Rd.xx, No. 7,  yy city',
-      receiverAddress:'Rd.zzz, No. 201,  yy city',
-      receiverName:'John Smith',
-      cardNumber: "756488521123",
-      size: "medium",
-      weight: "0.8",
-      description: "an iPad Pro",
-      deliveryMethod: "drone",
-      fee: "205",
-      status: "ongoing"
-  }
+    orderId: "12345",
+    senderAddress: "Rd.xx, No. 7,  yy city",
+    receiverAddress: "Rd.zzz, No. 201,  yy city",
+    receiverName: "John Smith",
+    cardNumber: "756488521123",
+    size: "medium",
+    weight: "0.8",
+    description: "an iPad Pro",
+    deliveryMethod: "drone",
+    fee: "205",
+    status: "ongoing",
+  };
 
-  trackingOnClick = (orderId) => {
-      getTrackingDetails(orderId)
-          .then((data) => {
-              this.setState(
-                  {
-                      senderAddress : data,
-                      receiverAddress : data,
-                      receiverName : data,
-                      cardNumber : data,
-                      size: data,
-                      weight: data,
-                      description: data,
-                      deliveryMethod: data,
-                      fee: data,
-                      status: data
-                  })
-          })
-          .catch((err) => {
-              message.error(err.message);
-          })
-  }
-  
+  trackingOnClick = orderId => {
+    getTrackingDetails(orderId)
+      .then(data => {
+        this.setState({
+          senderAddress: data,
+          receiverAddress: data,
+          receiverName: data,
+          cardNumber: data,
+          size: data,
+          weight: data,
+          description: data,
+          deliveryMethod: data,
+          fee: data,
+          status: data,
+        });
+      })
+      .catch(err => {
+        message.error(err.message);
+      });
+  };
+
   render = () => (
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />
-          <Menu theme="dark"  mode="horizontal" defaultSelectedKeys={['2']}>
-            <Menu.Item key="1">Shipping</Menu.Item>
-            <Menu.Item key="2">Tracking</Menu.Item>
-            <Menu.Item key="3">Register</Menu.Item>
-            <Menu.Item key="4">Log in</Menu.Item>
-          </Menu>
-        </Header>
-        <Content className='site-layout-background'
-                 style={{ padding: '0 50px' }}>
-          <div className="site-layout-content">
-              <Tracking
-                orderId={this.state.orderId}
-                //trackingOnClick={this.trackingOnClick}
-              ></Tracking>
-
-          </div>
-        </Content>
-        <Footer className='site-layout-footer' style={{ textAlign: 'center' }}>AutoExpress ©2021 </Footer>
-      </Layout>
-  )
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu.Item key="1">Ordering</Menu.Item>
+          <Menu.Item key="2">Tracking</Menu.Item>
+          <Menu.Item key="3">Register</Menu.Item>
+          <Menu.Item key="4">Log in</Menu.Item>
+        </Menu>
+      </Header>
+      <Content className="site-layout-background" style={{ padding: "0 50px" }}>
+        <div className="site-layout-content">
+          <Tracking
+            orderId={this.state.orderId}
+            //trackingOnClick={this.trackingOnClick}
+          ></Tracking>
+        </div>
+        <div>
+          <Ordering></Ordering>
+        </div>
+      </Content>
+      <Footer className="site-layout-footer" style={{ textAlign: "center" }}>
+        AutoExpress ©2021{" "}
+      </Footer>
+    </Layout>
+  );
 }
 /*
 function App() {
