e5d5fc3552a45c10e9429d7fdc93c75df8db5a47
simon zhang <zhang86036055@gmail.com>
2021-02-21 20:39:02 -0800

    Update ordering.js

--- FrontEnd/uavweb/src/components/ordering.js
+++ FrontEnd/uavweb/src/components/ordering.js
@@ -1,10 +1,52 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
-import { Input } from "antd";
+import {
+  Input,
+  Col,
+  Row,
+  Select,
+  InputNumber,
+  DatePicker,
+  AutoComplete,
+  Cascader,
+} from "antd";
+
+const { Option } = Select;
+const options = [
+  { value: "CA", lable: "CA" },
+  { value: "United States", lable: "United States" },
+];
 
 class Ordering extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: null,
+      lastName: null,
+      address1: null,
+      address2: null,
+      city: null,
+      state: "CA",
+      country: "United States",
+      zipCode: null,
+    };
+  }
+  handleSubmit = event => {
+    event.preventDefault();
+    const data = this.state;
+    console.log("Final data is", data);
+  };
+
+  handleInputChange = event => {
+    console.log(event.target.name);
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
+    const { firstName, lastName, address1, address2, city } = this.state;
+
     return (
       <div>
         <h1 style={{ textAlign: "center" }}> CHECKOUT </h1>
@@ -13,39 +55,77 @@ class Ordering extends Component {
           1. SHIPPING---------------- 2.BILLING------------------ 3.ORDER
           SUMMARY-------------------
         </h2>
-        <h1>
+
+        <h3 size="defalt size">From Address </h3>
+        <br />
+
+        <form onSubmit={this.handleSubmit} style={{ width: "100%" }}>
+          <Input.Group compact>
+            <Input
+              prefix={"First Name*"}
+              name="firstName"
+              onChange={this.handleInputChange}
+              style={{ width: "50%" }}
+              placeholder="Your First Name"
+            />
+            <Input
+              prefix={"Last Name*"}
+              name="lastName"
+              onChange={this.handleInputChange}
+              style={{ width: "50%" }}
+              placeholder="Your Last Name"
+            />
+          </Input.Group>
           <br />
+          <Input.Group compact>
+            <Input
+              prefix={"Address1*"}
+              name="address1"
+              onChange={this.handleInputChange}
+              placeholder="Street Number, street Name"
+            />
+
+            <Input
+              prefix={"Address2*"}
+              name="address2"
+              onChange={this.handleInputChange}
+              placeholder="Apartment number,suite number or company name"
+            />
+          </Input.Group>
           <br />
-          <h2>From Address </h2>
+          <Input.Group compact>
+            <Input
+              prefix={"City"}
+              name="city"
+              style={{ width: "80%" }}
+              onChange={this.handleInputChange}
+              placeholder="Street Number, street Name"
+            />
+            <Select
+              defaultValue="CA"
+              style={{ width: "5%" }}
+              onChange={this.handleInputChange}
+            >
+              <Option value="CA">CA</Option>
+            </Select>
+            <Select style={{ width: "15%" }} name="state">
+              <Option value="United States" onClick={this.handleInputChange}>
+                United States
+              </Option>
+            </Select>
+          </Input.Group>
           <Input
-            prefix="First Name"
-            style={{ width: "50%" }}
-            size="default size"
-            placeholder="First Name"
-          />
-          <Input
-            prefix="Last Name"
-            style={{ width: "50%" }}
-            size="default size"
-            placeholder="Last Name"
+            prefix={"Zip Code"}
+            name="zipCode"
+            onChange={this.handleInputChange}
+            style={{ width: "30%" }}
+            placeholder="within Great San Fransisco Area"
           />
 
-          <br />
-          <br />
-          <Input
-            style={{ width: "35%" }}
-            size="default size"
-            placeholder="Name"
-          />
-          <Input
-            style={{ width: "65%" }}
-            size="default size"
-            placeholder="Name"
-          />
-          <br />
-          <br />
-          <Input size="default size" placeholder="Name" />
-        </h1>
+          <p>
+            <button>Payment</button>
+          </p>
+        </form>
       </div>
     );
   }
