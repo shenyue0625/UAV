7c2572188ff9b7bc4d0fd2a939eac298f9736a9f
simon zhang <zhang86036055@gmail.com>
2021-02-21 22:22:21 -0800

    Update ordering.js

--- FrontEnd/uavweb/src/components/ordering.js
+++ FrontEnd/uavweb/src/components/ordering.js
@@ -1,21 +1,10 @@
 import React, { Component } from "react";
 import "antd/dist/antd.css";
-import {
-  Input,
-  Col,
-  Row,
-  Select,
-  InputNumber,
-  DatePicker,
-  AutoComplete,
-  Cascader,
-} from "antd";
+import { Input, Select, Button } from "antd";
+import makeAPayment from "../utils";
+import ButtonGroup from "antd/lib/button/button-group";
 
 const { Option } = Select;
-const options = [
-  { value: "CA", lable: "CA" },
-  { value: "United States", lable: "United States" },
-];
 
 class Ordering extends Component {
   constructor(props) {
@@ -28,9 +17,20 @@ class Ordering extends Component {
       city: null,
       state: "CA",
       country: "United States",
-      zipCode: null,
+      zipcode: null,
+
+      receiverFirstName: null,
+      receiverLastName: null,
+      receiverAddress1: null,
+      receiverAddress2: null,
+      receiverCity: null,
+      receiverState: null,
+      receiverCountry: null,
+      receiverZipcode: null,
+      size: null,
     };
   }
+
   handleSubmit = event => {
     event.preventDefault();
     const data = this.state;
@@ -38,15 +38,12 @@ class Ordering extends Component {
   };
 
   handleInputChange = event => {
-    console.log(event.target.name);
     this.setState({
       [event.target.name]: event.target.value,
     });
   };
 
   render() {
-    const { firstName, lastName, address1, address2, city } = this.state;
-
     return (
       <div>
         <h1 style={{ textAlign: "center" }}> CHECKOUT </h1>
@@ -109,9 +106,7 @@ class Ordering extends Component {
               <Option value="CA">CA</Option>
             </Select>
             <Select style={{ width: "15%" }} name="state">
-              <Option value="United States" onClick={this.handleInputChange}>
-                United States
-              </Option>
+              <Option value="United States">United States</Option>
             </Select>
           </Input.Group>
           <Input
@@ -123,9 +118,10 @@ class Ordering extends Component {
           />
 
           <p>
-            <button>Payment</button>
+            <button>Review</button>
           </p>
         </form>
+        <Button onClick={makeAPayment(this.state)}> Submit </Button>
       </div>
     );
   }
