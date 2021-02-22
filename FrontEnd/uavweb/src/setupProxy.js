396a98fab2e00dc120ad64995b295c443e78a277
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2021-02-22 18:18:24 +0800

    fix typo

--- FrontEnd/uavweb/src/setupProxy.js
+++ FrontEnd/uavweb/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.use(
     '/api',  // --> 凡是以'/api'开头的，都映射到'http://localhost:8080'
     createProxyMiddleware({
-      target: 'http://localhost:8080/',
+      target: 'http://localhost:8080',
       changeOrigin: true,
     })
   );
