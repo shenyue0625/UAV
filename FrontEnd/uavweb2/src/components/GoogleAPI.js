76a90628f14af47b2890efda703d89160aa40368
shenyue0625 <shen2299@stthomas.edu>
2021-03-01 23:03:03 +0800

    add getDistance method in GoogleAPI.js

--- FrontEnd/uavweb2/src/components/GoogleAPI.js
+++ FrontEnd/uavweb2/src/components/GoogleAPI.js
@@ -30,3 +30,35 @@ export const getLatAndLong = (address) => {
         console.log('err in fetch google api response -> ', error);
       })
 }
+
+//This is the function to calculate distance between 2 pairs of coordinates on map.
+function degreeToRadius(degree) {  return degree * Math.PI / 180; }
+
+export const getDistance = (lat1, lng1, lat2, lng2) => { //lat为纬度, lng为经度, 一定不要弄错
+  const EARTH_MEAN_RADIUS_KM = 6371.009;
+  const EARTH_MEAN_DIAMETER = EARTH_MEAN_RADIUS_KM * 2;
+  var dis = 0;
+  //计算纬度
+  var radLat1 = degreeToRadius(lat1);
+  var radLat2 = degreeToRadius(lat2);
+  //计算经度
+  var radLong1 = degreeToRadius(lng1);
+  var radLong2 = degreeToRadius(lng2);
+  var diffX = radLat1 - radLat2;//计算纬度差
+  var diffY = radLong1 - radLong2;//计算经度差
+  //计算正弦和余弦
+  var hsinX = Math.sin(diffX * 0.5);
+  var hsinY = Math.sin(diffY * 0.5);
+
+  //计算方式1：
+  // var latCenterRad_cos = Math.cos(lat1 * (Math.PI / 180));
+  // var h = hsinX * hsinX + (latCenterRad_cos * Math.cos(radLat2) * hsinY * hsinY);
+  // return (EARTH_MEAN_DIAMETER * Math.atan2(Math.sqrt(h), Math.sqrt(1 - h)));
+
+  //计算方式2：
+  //var dis = 6378137 * 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(diffX / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(diffY / 2), 2)));
+
+  //计算方式3：
+  var dis = 2 * Math.asin(Math.sqrt(Math.pow(hsinX, 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(hsinY, 2)));
+  return dis * 6378137;
+}
