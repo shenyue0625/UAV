53d8b63cc08013e88b857f7fcd5909fbac6884c4
simon zhang <zhang86036055@gmail.com>
2021-02-28 21:22:16 -0800

    Create GoogleAPI.js

---
+++ FrontEnd/uavweb2/src/components/GoogleAPI.js
@@ -0,0 +1,9 @@
+//this google.js file is to implement all google app API and return the numbers per needed.
+//Google API KEY:  AIzaSyB53y4-6k1lvgsORyVe1T28zH82VkpFmrA  feel free to use
+export const getDistance = (origin, distination) => {
+  return 1;
+};
+
+export const getRoute = (origin, distination) => {
+  return 2;
+};
